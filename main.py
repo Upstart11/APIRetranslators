e1b3796a047605e534802af1150d78b97f3a6f0c
Ciocoi Vladislav <vladislav.siloci1@gmail.com>
2024-06-21 11:14:12 +0300

    Added code to create Images directory if not existent

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from fastapi.responses import FileResponse
 from uncrop import getresult
 import uvicorn
 import io
+import os
 from PIL import Image
 from pathlib import Path
 
 app = FastAPI()
 
+IMAGES_DIR = "Images"
 
-INPUT_DIR = "Images/input.png"
+
+INPUT_FILE = "Images/input.png"
 INPUT_PATH = Path("Images/input.png")
 
-OUTPUT_DIR = "Images/output.png"
+OUTPUT_FILE = "Images/output.png"
 OUTPUT_PATH = Path("Images/output.png")
 
 
@@ -31,14 +34,16 @@ async def uncropEP(
     positiveprompt: str = Form(...),
     negativeprompt: str = Form(...)
 ):
-    
+    if not os.path.exists(IMAGES_DIR):
+        os.makedirs(IMAGES_DIR)
+
     image_data = await file.read()
     Inputimage = Image.open(io.BytesIO(image_data))
-    Inputimage.save(INPUT_DIR)
+    Inputimage.save(INPUT_FILE)
     
     # Save the uploaded file
     responseimg = getresult(left, top, right, bottom, positiveprompt, negativeprompt)
-    responseimg.save(OUTPUT_DIR)
+    responseimg.save(OUTPUT_FILE)
 
     # Return the processed image
     return FileResponse(OUTPUT_PATH, media_type="image/png", filename=OUTPUT_PATH.name)
